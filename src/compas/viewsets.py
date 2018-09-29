253b51730064b7578a747c96952e326d0f2b5b5b
Norton Pengra <npengra317@gmail.com>
2018-09-29 10:47:57 -0700

    please

--- src/compas/viewsets.py
+++ src/compas/viewsets.py
@@ -2,13 +2,15 @@ from rest_framework import viewsets, permissions, pagination
 from compas.models import Assessment
 from compas.serializers import AssessmentSerializers
 
+class Pagination(pagination.PageNumberPagination):
+    page_size = 100
+    max_page_size = 1000
 
 class AssessmentViewset(viewsets.ModelViewSet):
     queryset = Assessment.objects.all()
     serializer_class = AssessmentSerializers
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
-    pagination_class = pagination.PageNumberPagination
-    page_size = 100
+    pagination_class = Pagination
     lookup_field = 'assessment_id'
     search_fields = [
         'date',
